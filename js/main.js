f0f2d1a23468f09ac0fe8fe0f35ba609c598b6f0
AlinaKhaff <Tigrangel@gmail.com>
2021-05-05 18:40:07 +0300

    TZ-13

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive:{
+            0:{
+                items:3
+            },
+            320:{
+                items:1
+            },
+            480:{
+                items:1
+            },
+            768:{
+                items:3
+            }
+        }
+    });
+  
