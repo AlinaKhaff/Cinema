8a04ba2bd624f53c6cb426038c2cd5f54c90e687
AlinaKhaff <Tigrangel@gmail.com>
2021-05-11 10:17:53 +0300

    TZ16

--- js/film-table.js
+++ js/film-table.js
@@ -18,83 +18,96 @@ const tableHeader = `
                         <p>Жанр</p>
                     </a>
 `
-const films =[
-    {
-        start: '10:00',
-        title: ' Человек паук',
-        genre: [
-            {
-                name: 'Фантастика',
-            },
-            {
-                name:'боевик',
-            },
-            {
-                name:'приключения',
-            },
-        ]
-    },{ 
-        start: '12:00',
-        title: 'Собачья жизнь 2 ',
-        genre: [
-            {
+const films = [{
+   
+    start: '01:00',
+    title: ' XXX ',
+    adult: true,
+    genre: []
+},{
+   
+    start: '10:00',
+    title: ' Человек паук',
+    adult: false,
+    genre: [{
+            name: 'Фантастика',
+        },
+        {
+            name: 'боевик',
+        },
+        {
+            name: 'приключения',
+        },
+    ]
+}, {
+    
+    start: '12:00',
+    title: 'Собачья жизнь 2 ',
+    genre: [{
             name: 'Фэнтэзи',
-            },
-            {
-            name:'драма',
-            },
-            {
-            name:'комедия',
-            },
-        ]
-    },{
-        start: '14:00',
-        title: ' История игрушек 4',
-        genre: [
-            {
+        },
+        {
+            name: 'драма',
+        },
+        {
+            name: 'комедия',
+        },
+    ]
+}, {
+    
+    start: '14:00',
+    title: ' История игрушек 4',
+    genre: [{
             name: 'Мультфильм',
-            },
-            {
-            name:'фэнтэзи',
-            },
-            {
-            name:'комедия',
-            },
-        ]
-    },{
-        start: '16:00',
-        title: ' Люди в чёрном: Интэрнэшнл',
-        genre: [
-            {
+        },
+        {
+            name: 'фэнтэзи',
+        },
+        {
+            name: 'комедия',
+        },
+    ]
+}, {
+
+    start: '16:00',
+    title: ' Люди в чёрном: Интэрнэшнл',
+    genre: [{
             name: 'фантастика',
-            },
-            {
-            name:'боевик',
-            },
-            {
-            name:'комедия',
-            },
-        ]
-    }
-];
+        },
+        {
+            name: 'боевик',
+        },
+        {
+            name: 'комедия',
+        },
+    ]
+}];
+
+function renderFilmTableItem(film, clazz) {
+    return `
+    <p class=" block03__line-svg ${clazz}" > 
+    <input type="checkbox" class="block_03_checkbox" id="${film.title.replaceAll(' ', '-')}">
+        <label for="${film.title.replaceAll(' ', '-')}">
+            <svg width="11" height="9" viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
+                    fill="white" />
+            </svg>
+        </label>
+    </p>
+
+    <span class="${clazz}">${film.start}</span>
+    <a href="https://www.kinopoisk.ru/film/838/" class="${clazz}" target="_blank">
+        ${film.title}
+    </a>
+     <span class="${clazz}">${film.genre.map(item => item.name).join(', ')}</span>
+`
+}
 const div = document.getElementById('block03__table');
 div.innerHTML = tableHeader;
 for (let index = 0; index < films.length; index++) {
+    if (!films[index].adult) {
     const clazz = (index % 2 === 0) ? 'block03__line2' : 'block03__line1';
-    const element = films[index];
-    div.innerHTML += `
-        <p class="block03__line-svg ${clazz} ">
-                                <svg width="11" height="9" viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                        d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                                        fill="white" />
-                                </svg>
-                            </p>
-
-        <span class="${clazz}">${element.start}</span>
-        <a href="https://www.kinopoisk.ru/film/838/" class="${clazz}" target="_blank">
-                            ${element.title}
-        </a>
-        <span class="${clazz}">${element.genre.map(item => item.name).join(', ')}</span>
-    `    
+    div.innerHTML += renderFilmTableItem(films [index], clazz);
+   }
 }
